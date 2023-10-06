2f82204c1917fa7618ffb647d76e5db10f6a864b
Nalin Kumar <nalin12894+github@gmail.com>
2023-10-06 08:42:38 +0530

    evaluated expiation and createElement example

--- vit_react/src/App.jsx
+++ vit_react/src/App.jsx
@@ -1,7 +1,15 @@
 import Test from "./Test";
 
 function App() {
-  return <Test />;
+  const username = "Nalin Kumar"; // evaluated expiation
+  return (
+    <>
+      <Test />
+      <h3>
+        hello from app with variable user name <i>{username}</i>!
+      </h3>
+    </>
+  );
 }
 
 export default App;
--- vit_react/src/main.jsx
+++ vit_react/src/main.jsx
@@ -1,9 +1,29 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from "./App.jsx";
+// function MyApp() {
+//   return <h3>Custom App !!</h3>;
+// }
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://www.google.com",
+//     target: "_blank",
+//   },
+//   children: "click me to visit google",
+// };
+// const anotherElement = (
+//   <a href="https://www.google.com" target="_blank">
+//     visit google
+//   </a>
+// );
+// const anotherVariable = " test variable";
+// const reactElement = React.createElement(
+//   "a",
+//   { href: "https://github.com", target: "_blank" },
+//   "click me visit google",
+//   anotherVariable // evaluated expiation
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);

261c2c8c6ed4caf22105b9436b8b638379cac3a3
Nalin Kumar <nalin12894+github@gmail.com>
2023-10-06 09:57:04 +0530

    useState practice and poc

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(15);
+
+  // const [counter, nk] = useState(15); // this can also be used
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+  const remove = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+  return (
+    <>
+      <h1> React practice</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button onClick={remove}>remove value {counter}</button>
+    </>
+  );
+}
+
+export default App;

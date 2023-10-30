6b6a11c02bfcedd50125e0bd7eed519418bd5048
Nalin Kumar <nalin12894+github@gmail.com>
2023-10-30 23:05:26 +0530

    added import statement

--- 12_blog_app/src/components/Header/Header.jsx
+++ 12_blog_app/src/components/Header/Header.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Container, Logo, LogoutBtn } from "../index";
-// import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-// import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const navItems = [
     {
@@ -19,7 +18,7 @@ function Header() {
       enabled: !authStatus,
     },
     {
-      name: "Signup",
+      name: "SignUp",
       ulr: "/signup",
       enabled: !authStatus,
     },
